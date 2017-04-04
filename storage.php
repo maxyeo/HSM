e901f82f95915e881035e962c688310a457fcae6
Max Yeo <mzwyeo@gmail.com>
2017-04-04 14:49:38 -0400

    added policy pdf and updated faw

--- storage.php
+++ storage.php
@@ -26,6 +26,7 @@ Template Name: Storage
             <div class="main-content">
                 <h1>Summer Storage With HSM</h1>
                 <h2>the easiest thing ever</h2>
+                <p class="hero-content">For more information on summer storage, please see our procedure and policies <a href="<?php echo get_stylesheet_directory_uri(); ?>/static/summer.pdf">here</a> or below.</p>
 
                 <div class="col-3-4 push-2 video">
                     <div class="video-wrapper">
@@ -205,51 +206,36 @@ Template Name: Storage
             <div id="storage-faq" class="grid cf">
                 <h2 class="col-1-1">Frequenty Asked Questions</h2>
                 <div class="col-1-2 faq">
-                    <h5>What do I need to do before my pick-up appointment?</h5>
-                    <p>The following packing guidelines will help ensure that your items are not harmed over the summer. 
-                    Wrap all items with bubble wrap or paper so that they fit snugly inside your boxes.
-                    Label all items on ALL SIDES
-                    We will be stacking your boxes so fill them up and use adequate cushioning material.
-                    Tape drawers shut if you plan on storing plastic drawers.
-                    Make sure to defrost and dry your refrigerator prior to your pick-up appointment.
-                    Do not pack any liquids!
-                    If you are storing microwaves or toasters, make sure to tape their cords to the box.
-                    Wrap any loose items that you plan on storing.
-                    Seal your boxes!  Makes sure to tape both the top and bottom of your boxes.
-                    Make sure that your boxes do not exceed 70lbs.  We will charge you an extra $10 per box that weighs more than 70lbs.
-                    A $5 service charge will apply to any item that has not been packaged by these guidelines.
-                    </p>
-                </div>
-                <div class="col-1-2 faq">
-                    <h5>How do I schedule a pick-up appointment?</h5>
-                    <p>You can schedule a pick-up appointment through our online scheduling system.</p>
-                </div>
-                <div class="col-1-2 faq">
-                    <h5>Do I need to meet the movers anywhere?  What do I do on the day of my pick-up appointment?</h5>
-                    <p>Our movers will come right to your room. All you need to do before your pick-up appointment is make sure that all of your items are packed up.<br>
-                    If you do not answer the door, we will call you at the phone number you provide when you schedule your appointment.<br>
-                    If you miss your appointment we will try our best to accommodate a rescheduling.<br>
-                    Appointments cancelled within 5 days of the appointment date will be charged a cancellation fee of $20.</p>
+                    <h5>What should I include on my labels?</h5>
+                    <p>You should include: Your full name as it appears on your appointment invoice, your phone number, and the dorm/building you are moving into for the <b>Fall 2017 semester.</b></p>
                 </div>
                 <div class="col-1-2 faq">
                     <h5>How do I pay?</h5>
-                    <p>You can pay immediately after your pick-up appointment with either a credit card or a check.  The check should be made out to Hopkins Student Movers.  We do not accept cash.</p>
-                </div>
-                <div class="col-1-2 faq">
-                    <h5>When will I get my stuff back? </h5>
-                    <p>We will e-mail you once we finalize our move in schedule.  You will then be able to schedule a redelivery appointment the same way you scheduled your pick-up appointment.</p>
-                </div>
-                <div class="col-1-2 faq">
-                    <h5>What should the labels on my items include?</h5>
-                    <p>Labels should include your full name, your phone number, and the address you want your items to be re-delivered to after the summer is over. If you are moving into a dorm, just the dorm name and room number will suffice. If not, please provide the full address.</p>
-                </div>
-                <div class="col-1-2 faq">
-                    <h5>How long do I have to sign up for an appointment?</h5>
-                    <p>Standard registration will go until Friday, April 28, 2017. After this date, appointments can still be scheduled until Friday, May 5. However, any appointments scheduled between Saturday, April 29, and Friday, May 5 will be subject to a 10% price increase late fee. May 5 is the absolute deadline to schedule an appointment. No appointments will be accepted past Friday May 5.</p>
+                    <p>You can pay for your storage after your items are picked up. We will have a table set up in Wolman hall to collect payments. We accept: cash, credit, check, or J-cash for payment.</p>
                 </div>
                 <div class="col-1-2 faq">
                     <h5>Where can I get boxes?</h5>
-                    <p>Boxes will available for purchase in Wolman during the time of move out, and at Fedex in Hopkins Square.</p>
+                    <p>Boxes can be purchased from the Wolman Mail Room. The Wolman Mail Room also excepts J-cash. Boxes can also be purchased at the FedEx store under Homewood dorm.</p>
+                </div>
+                <div class="col-1-2 faq">
+                    <h5>Why do I need to pay extra if my boxer is greater than 20x20x20?</h5>
+                    <p>It is much more difficult to organize boxes of various sizes in our storage units.</p>
+                </div>
+                <div class="col-1-2 faq">
+                    <h5>Why is there only a discount for multiple boxes, and not plastic items?</h5>
+                    <p>Boxes are the easiest and most convenient item to store. Plastic containers, are more likely to break, and much more difficult to stack and organize than boxes, so we impose the discount to encourage students to store their items in boxes if possible.</p>
+                </div>
+                <div class="col-1-2 faq">
+                    <h5>If my item is on the pricing list, but I choose to store it in a box, will it be charged as that item, or as a box?</h5>
+                    <p>If you are storing an item in it’s original box, (i.e, Microwave, Fan, TV, etc.) it will be charged as the item. If you store the item inside a box other than it’s original box, it will be charged as a box.</p>
+                </div>
+                <div class="col-1-2 faq">
+                    <h5>How do I get my items back?</h5>
+                    <p>Over the summer, we will finalize our move in dates. Much like move out, you will be able to book a move-in appointment for one of these days</p>
+                </div>
+                <div class="col-1-2 faq">
+                    <h5>What if I don’t see an item I am planning on storing on the pricing sheet?</h5>
+                    <p>The pricing sheet on our website includes the most commonly stored items. If you are storing an item that is not on the list, please contact us and we will let you know how much it would cost to store. We determine the price of unlisted items by their size, weight, and storability</p>
                 </div>
             </div>
 
